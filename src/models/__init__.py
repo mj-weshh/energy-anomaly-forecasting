3188e7d4bd3bd2a2238d565b73f5056386d65dc3
Waweru Muhura <jowamumjwesh@gmail.com>
2026-07-06 13:18:06 +0200

    chore: setup models directory and evaluation metrics module

---
+++ src/models/__init__.py
@@ -0,0 +1,13 @@
+"""Unsupervised anomaly detection models and evaluation utilities.
+
+Public API is defined in submodules. Import directly::
+
+    from src.models.evaluate_models import evaluate_anomaly_model
+
+Training functions will be added in :mod:`src.models.train_anomaly_models`
+during Phase 2, Week 4.
+"""
+
+__all__ = [
+    "evaluate_anomaly_model",
+]
